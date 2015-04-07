f8f7b4e3d1700966966ef49497ef6e765b250734
alfandersen <alf.andersen.1986@gmail.com>
2015-04-07 11:51:36 +0200

    New prototype07

---
+++ Prototype07.cpp
@@ -0,0 +1,100 @@
+//#include "allocore/io/al_App.hpp"
+#include "Gamma/Oscillator.h"
+#include "Gamma/Envelope.h"
+#include "Gamma/Delay.h"
+#include "Gamma/Filter.h"
+#include "Gamma/Noise.h"
+#include "Gamma/SamplePlayer.h"
+#include "header/al_AudioApp.hpp"
+#include "header/Arduino.hpp"
+#include "header/Sound.hpp"
+using namespace al;
+
+class MyApp : public AudioApp{
+public:
+	static const int A = 2;
+	Arduino arduino[A];
+	Values values;
+	Synth fmSynth[A];
+	int currentRead;
+	int meanArray;
+	double time;
+	
+	MyApp():
+	values(A,2,3,"COM3",19200) // arduino, sensor, amount of values pr sensor, COMPORT, baudrate 
+	//values(A,2,3,"/dev/tty.usbmodem1411",19200) // arduino, sensor, amount of values pr sensor, COMPORT, baudrate 
+	{
+		initAudio(44100,128,2,0);
+		currentRead = 0;
+		meanArray = 2500;
+		time = 0;
+	}
+
+	// Audio callback
+	virtual void onSound(AudioIOData& io){	
+		gam::sampleRate(io.fps());
+	
+		float index = ((arduino[0].angleX + 90) / 180.) * 20;
+		float ratio = ((arduino[0].angleY + 90) / 180.) * 5;
+		
+		while(io()){
+			
+			fmSynth[0].volume[currentRead] = fmSynth[0].currentVolume(arduino[0].veloX, arduino[0].veloY, arduino[0].veloZ);	
+			fmSynth[1].volume[currentRead] = fmSynth[1].currentVolume(arduino[1].veloX, arduino[1].veloY, arduino[1].veloZ);	
+			
+			currentRead++;
+			if(currentRead == meanArray){
+				currentRead = 0;		
+			}
+			
+			float volume[2];
+			volume[0] = fmSynth[0].averageFilter(fmSynth[0].volume, meanArray);
+			volume[1] = fmSynth[1].averageFilter(fmSynth[1].volume, meanArray);
+			
+			float fc = 1000 * volume[0] + 1;
+			float out[2];
+			out[0] = fmSynth[0].modulate(fc, ratio, index) * volume[0];
+			//out[1] = fmSynth[1].modulate(fc1, ratio1, index1) * volume[1];
+			//out[1] = fmSynth[1].wavOsc(arduino[1].angleX, arduino[1].angleY);
+			//fmSynth[1].clip(volume[1],1,0);
+			float delay[2];
+			float delayTime = (arduino[1].angleX / 90.) + 1;
+			fmSynth[0].clip(delayTime,2,0);
+			float feedback = (arduino[1].angleY / 180.) + 0.5;
+			fmSynth[0].clip(feedback,1,0);
+			
+			delay[0] = fmSynth[0].echo(out[0], delayTime, feedback, 10000);
+			//delay[1] = fmSynth[1].echo(out[1], 1.1 - volume[1], .5, 15000);
+			
+			//float outs = out[1] + delay;
+			//fmSynth[1].clip(outs,0.5,-0.5);
+			//io.out(0) = outs;
+			//io.out(1) = outs;
+			
+			io.out(0) = out[0] + delay[0];
+			io.out(1) = out[0] + delay[0];
+			
+		}
+	} 
+
+	// Graphics callbacks
+	virtual void onAnimate(double dt){
+		
+		if(values.unpack()){
+			for(int i = 0; i < 2; i++){
+				arduino[i].getValues(i, values.arduino);
+				arduino[i].setAngles(dt);
+				arduino[i].velocity(dt);
+			}
+			//std::cout<<"acc: "<<arduino[0].acc[0]<<"     "<<arduino[0].acc[1]<<"     "<<arduino[0].acc[2]<<"\n";
+			//std::cout<<arduino[0].angleX<<"     "<<arduino[0].angleY<<"\n";
+		}
+		
+		
+	}
+};
+
+
+int main(){
+	MyApp().start();
+}
