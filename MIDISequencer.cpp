9539a8d91ac866a907912db7be2afeb7c7678539
NikolajAndersson <nikolajandersson@gmail.com>
2015-04-08 20:57:15 +0200

    added Midi sequencer

--- MIDISequencer.cpp
+++ MIDISequencer.cpp
@@ -7,224 +7,167 @@
 
 using namespace al;
 
-/*
-	WaveTable, frequency modulation, synth class, mouse control
-*/
-
-class Synth {
+class Sequencer{
 public:
-	gam::Sine<> src[8], mod, car[128];	
-	gam::Osc<> osc[128], carOsc[128];
-	gam::ArrayPow2<float> table[4];	// Wavetable
+	gam::Sine<> src[8];
 	gam::ADSR<> env[8];
 	gam::Accum<> tmr;
+	
+	Mesh quad[8][8];
 
+	bool KeyOn[8][8], start;
+	int pentaScale[8], counter, noteOn;
+	float color[8], velocity[128];
 	
-	bool modulate, KeyOn[8][8], KeyOff[128], waveform, seqOn;
-	float freq[128], note[128], velocity[128], oldVelo[128], mousePosX, mousePosY, seqDuration;
-	int wavetable;
-	int keyAmount, noteOn, noteOff, counter, pentaScale[8];
-	
-	Synth(){
-		std::cout << "MIDISynthesizer!!" << "\n";
-		std::cout << "controls: " << "\n";
-		std::cout << "z - enable and disable wavetable synth" << "\n";
-		std::cout << "m - enable and disable FM - Use mouse to control ratio and index" << "\n";
-		std::cout << "wavetable synthesis: 0 - Clausen function, 1 - Square wave, 2 - Saw wave, 3 - Triangle wave " << "\n";
-		//keyAmount = 127;
-		noteOn = noteOff = modulate = waveform = seqOn = 0;
-		for(int i = 0; i <= keyAmount; i++){
-			freq[i] = (pow(2., ((i - 69) / 12.))) * 440;
+	Sequencer(){
+		counter = 0;
+		start = false;
+		tmr.freq(4);
+		pentaScale[0] = 69;
+		pentaScale[1] = 72;
+		pentaScale[2] = 74;
+		pentaScale[3] = 76;
+		pentaScale[4] = 79;
+		pentaScale[5] = 81;
+		pentaScale[6] = 84;
+		pentaScale[7] = 86;
+		noteOn = 0;
+		
+		for(int i = 0; i < 128; i++){
+			velocity[i] = 0;
+		}
+		
+		for(int i = 0; i < 8; i++){
+			src[i].freq((pow(2., ((pentaScale[i] - 69) / 12.))) * 440);
 			env[i].sustainDisable();
 			env[i].attack(1./1000);
-			env[i].decay(1./1000);
-			env[i].sustain(0.8);
+			env[i].decay(0.05);
+			env[i].sustain(0.5);
 			env[i].release(0.2);
-			KeyOn[i] = 0;
-			KeyOff[i] = 1;
-			note[i] = 0;
-			velocity[i] = 0;
-			oldVelo[i] = 0;
-			src[i].freq(freq[i]);
-			osc[i].freq(freq[i]);
-		}
-		tmr.freq(20);
-		// set variables
-		mousePosX = 1;
-		mousePosY = 1;
-		wavetable = 0;
-		
-		//pentaScale[] = {69, 72, 74, 76, 79, 81, 84, 86}
-		
-		
-		for(int i = 0; i < 4; i++){
-			table[i].resize(2048);
-		}
-		
-		for(int k=1; k<=16; k++){
-			gam::addSine(table[0], k, 1./(k*k)); // Clausen function
-			gam::addSine(table[1], 2*k-1, 1./(2*k-1)); // square wave
-			gam::addSine(table[2], k, 1./k); // saw wave
-			if(k%2) gam::addSine(table[3], k, 1./(k*k), 0.25); // triangle wave
-		}	
+			for(int j = 0; j < 8; j++){
+				KeyOn[j][i] = false;
+				quad[j][i].primitive(Graphics::QUADS);
+				quad[j][i].vertex(2.*(j/7.) - 1.0 , 2.*(i/7.) - 1.0);
+				quad[j][i].vertex(2.*(j/7.) - 0.8 , 2.*(i/7.) - 1.0);
+				quad[j][i].vertex(2.*(j/7.) - 0.8 , 2.*(i/7.) - 0.8);
+				quad[j][i].vertex(2.*(j/7.) - 1.0 , 2.*(i/7.) - 0.8);
+			}
+		}		
 	}
-	// where all the magic happens.
-	// Frequency, timbre and waveform
+	
 	void check(){
-		for(int i = 0; i <= keyAmount; i++){
-				if(KeyOn[i]){	
-					note[i] = src[i]();	
-					}
-				}
-			//if(seqOn){
-				if(counter == 72) counter = 64;
-					//std::cout << "hallo" << "\n";
-					//tmr.freq(seqDuration * 100 + 20);
-				if(tmr()){
-					//env[counter].reset();
-					//std::cout << "hallo" << "\n";
-					counter++;
-					}
-				for(int i = 0; i < 8; i++){
-					if(KeyOn[counter][i]) env[i].reset();
-				}						
-				//}
-		}
-	// manage the output	
-	float output(float velocity[], float volume = 0.1){
-		float out = 0;
-			for(int i = 0; i < 8; i++){
-					out += note[i]  * env[i]();
-				
-			}
-		out *= volume;	
-		return out;
-	}
-		
-	void KeyPress(const Keyboard& k){
-		switch(k.key()){
-		
-		case 'w':
-			if(!waveform){
-				waveform = 1;
-				std::cout<< "Wavetable mode enabled" << "\n";
-			} else if (waveform){
-				waveform = 0;
-				std::cout<< "Sine mode enabled" << "\n";
-			}
-		break;
-		case 's':
-			if(!seqOn){
-				seqOn = 1;
-				std::cout<< "Sequencer mode enabled" << "\n";
-			} else if (seqOn){
-				seqOn = 0;
-				std::cout<< "Sequencer mode disabled" << "\n";
-			}
-		break;
-		case 'm':
-			if(!modulate){
-				modulate = 1;
-				std::cout<< "Frequency modulator enabled" << "\n";
-			} else if(modulate){
-				modulate = 0;
-				std::cout<< "Frequency modulator disabled" << "\n";			
-			}
-		break;
-		case '0':
-			std::cout<< "Clausen function" << "\n";
-			wavetable = 0;
-		break;	
-		case '1':
-			std::cout<< "Square wave" << "\n";
-			wavetable = 1;
-		break;
-		case '2':
-			std::cout<< "Saw wave" << "\n";
-			wavetable = 2;
-		break;		
-		case '3':
-			std::cout<< "Triangle wave" << "\n";
-			wavetable = 3;
-		break;	
-				
+		counter++;
+		if(counter == 8) counter = 0;
+		for(int i = 0; i < 8; i++){
+			if(KeyOn[i][counter]) env[i].reset();
 		}
 	}
 	
-	void Mouse(const ViewpointWindow& w, const Mouse& m){
-		seqDuration = float(float(m.x()) / w.width());
-		mousePosX = float(m.x());
-		mousePosY = float(m.y());
-	}	
+	// manage the output	
+	float output(float volume = 0.1){
+		float out = 0;
+		for(int i = 0; i < 8; i++){
+			color[i] = env[i]();
+			out += src[i]() * color[i];
+			
+		}
+		out *= volume * start;	
+		return out;
+	}
 
+	void getMousePos(const ViewpointWindow& w, const Mouse& m, Vec3d cam){
+		start = true;
+		// We have narrowed the camera lens constant down to be 0.26795
+		float cX = cam.z * 0.26795 * w.aspect();
+		float cY = cam.z * 0.26795;
+		
+		// Convert the pixel values to coordinates
+		float mouseX = ( (2. * cX / (cX - cam.x)) * m.x() / w.width()  - 1) * (cX - cam.x);
+		float mouseY = (-(2. * cY / (cY + cam.y)) * m.y() / w.height() + 1) * (cY + cam.y);
+		
+		
+		
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				if(mouseX > quad[i][j].vertices()[0].x && mouseX < quad[i][j].vertices()[2].x){
+					if(mouseY > quad[i][j].vertices()[0].y && mouseY < quad[i][j].vertices()[2].y){
+						KeyOn[j][i] = !KeyOn[j][i];
+					}
+				}
+			}
+		}
+	}
+	
+	void draw(Graphics& g){
+		for(int i = 0; i < 8; i++){
+			for(int j = 0; j < 8; j++){
+				if(KeyOn[j][i]){
+					if(counter == i) g.color(HSV(0, 0, 0.5+color[j]*0.5));
+					else g.color(HSV(0, 0, 1));
+				}
+				else g.color(HSV(0,0,0.3));
+				g.draw(quad[i][j]);
+			}
+		}
+	}
 	void MIDI(const MIDIMessage& m){
 
 		// Here we demonstrate how to parse common channel messages
 		switch(m.type()){
-		case MIDIByte::NOTE_ON:
-			noteOn = m.noteNumber();
-			velocity[noteOn] = m.velocity();
-		if(velocity[noteOn] == 1){
-			KeyOn[noteOn/16][noteOn%16] = !KeyOn[noteOn/16][noteOn%16];
-			std::cout << "y: " << noteOn/16 << "   x: " << noteOn%16 << "\n";
-			//KeyOff[noteOn] = !KeyOff[noteOn];
-			
-				} 
-					break;
+		case MIDIByte::NOTE_ON:	noteOn = m.noteNumber(); velocity[noteOn] = m.velocity();
+			if(velocity[noteOn] == 1){
+				KeyOn[7 - noteOn/16][noteOn%16] = !KeyOn[7 - noteOn/16][noteOn%16];	
+			} break;
 		}
 	}	
-	
 };
 
 class MyApp : public App, public MIDIMessageHandler{
 public:
-	Synth synth;
+	Sequencer seq;
 	MIDIIn midiIn;
+	Vec3d cam;
 
 	MyApp(){
-		nav().pos(0,0,4);
-		//initWindow();
+		nav().pos(0,0,5);
+		cam = nav().pos();
+		initWindow();
 		initAudio();
-		//window().remove(navControl());
+		window().remove(navControl());
 		
 		if(midiIn.getPortCount() > 0){
-
+			
 			// Bind ourself to the MIDIIn
 			MIDIMessageHandler::bindTo(midiIn);
 
 			// Open the last device found
 			int port = midiIn.getPortCount()-1;
-			midiIn.openPort(0);
+			midiIn.openPort(port);
 			printf("Opened port to %s\n", midiIn.getPortName(port).c_str());
 		}
-		else{
-			printf("Error: No MIDI devices found.\n");
-		}
+		else printf("Error: No MIDI devices found.\n");	
 	}
 
 	void onSound(AudioIOData& io){
 		gam::sampleRate(io.fps());
 		
 		while(io()){
-			synth.check();
-		
-			float out = synth.output(synth.velocity);	
+			if(seq.tmr()) seq.check();
+			float out = seq.output();	
 	
 			io.out(0) = out;
 			io.out(1) = out;	
 		}
 	}
-	// Keyboard/mouse input callbacks
-	virtual void onKeyDown(const ViewpointWindow& w, const Keyboard& k){
-		synth.KeyPress(k);
-	}
-
-	virtual void onMouseMove(const ViewpointWindow& w, const Mouse& m){
-		synth.Mouse(w, m);
-	}
 
 	void onMIDIMessage(const MIDIMessage& m){
-		synth.MIDI(m);	
+		seq.MIDI(m);	
+	}
+	virtual void onMouseDown(const ViewpointWindow& w, const Mouse& m){
+		seq.getMousePos(w, m, cam);
+	}
+	void onDraw(Graphics& g, const Viewpoint& v){
+		seq.draw(g);
 	}
 };
 
