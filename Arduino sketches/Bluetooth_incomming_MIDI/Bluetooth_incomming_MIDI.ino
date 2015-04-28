62a5bbe56a34726989f1cbe2eb3f11d8da360998
NikolajAndersson <nikolajandersson@gmail.com>
2015-04-28 13:09:16 +0200

    added MIDI-server

--- Arduino sketches/Bluetooth_incomming_MIDI/Bluetooth_incomming_MIDI.ino
+++ Arduino sketches/Bluetooth_incomming_MIDI/Bluetooth_incomming_MIDI.ino
@@ -1,10 +1,78 @@
-/* Upload this sketch into Seeeduino and press reset*/
+#include <Kalman.h>
 #include <MIDI.h>
 #include <SoftwareSerial.h>   //Software Serial Port
-#include <BAP.h> //Byte ArrgyroY Protocol
 
 #define RxD 7
 #define TxD 6
+
+class Arduino{
+public:
+  float gyro[3];
+  float acc[3];
+  Kalman kalmanX,kalmanY;
+  float angleX,angleY,angleZ, oldAngleX, oldAngleY, oldAngleZ, veloX, veloY, veloZ;
+
+	
+  Arduino(){
+    for(int i = 0; i < 3; i++){
+	 gyro[i] = 0;
+	  acc[i] = 0;
+	 }
+     angleX = angleY = angleZ = oldAngleX = oldAngleY = oldAngleZ = veloX = veloY = veloZ = 0;
+		//kalmanX.setQangle(0.01); //0.001 default
+		//kalmanX.setQbias(0.03); //0.003 default
+		//kalmanX.setRmeasure(0.3); //0.03 default
+	}
+	
+     void getValues(int ID, unsigned char arduino[][2][3]){
+       for(int s = 0; s < 2; s++){ 
+         for(int v = 0; v < 3; v++){
+           if(s == 0){
+             gyro[v] = arduino[ID][s][v] - 127;
+             }
+           if(s == 1){
+             acc[v] =  ((arduino[ID][s][v] - 127) / 127.) * 1.52;
+             }
+           }
+         }
+       }
+	
+  void setAngles(double dt){
+     double roll = (atan2(float(acc[0]), float(acc[2])) / (2.*M_PI)) * 360.;
+     double pitch = (atan2(float(acc[1]), float(acc[2])) / (2.*M_PI)) * 360.;
+     float rateX = -gyro[0]*20;
+     float rateY = -gyro[1]*20;
+        		
+	if(acc[2] < 0){
+	  if(roll < 0){
+	     roll = -180-roll;
+	    }else{
+	      roll = 180-roll;
+	}
+	if(pitch < 0){
+          	pitch = -180-pitch;
+        } else{
+	  pitch = 180-pitch;
+	}
+    }
+    angleX = kalmanX.getAngle(roll, rateX, dt);
+    angleY = kalmanY.getAngle(pitch, rateY, dt);
+    angleZ += gyro[2];
+    //angleZ *= 0.99;
+  }
+	
+  void velocity(double dt){
+	
+    veloX = fabs(angleX - oldAngleX)/ dt;
+    veloY = fabs(angleY - oldAngleY)/ dt;
+    veloZ = fabs(angleZ - oldAngleZ)/ dt;
+    oldAngleX = angleX;
+    oldAngleY = angleY;
+    oldAngleZ = angleZ;
+  }
+};
+
+Arduino ard[4];
 
 String retSymb = "+RTINQ=";//start symble when there's any return
 String slaveName = ";SeeedBTSlave";//Set the Slave name ,caution that ';'must be included
@@ -19,22 +87,23 @@ String connectCmd = "\r\n+CONN=";
 SoftwareSerial blueToothSerial(RxD,TxD);
 
 //------------
-unsigned char seq = 0; // sequence number
+//unsigned char seq = 0; // sequence number
 
 const int arduinoAmount = 2;
 const int sensorAmount = 2;
 const int valuesPrSensor = 3;
 
 unsigned long time = 0;
+unsigned long oldTime = 0;
 unsigned long lastTime = 0;
 
-const int len = arduinoAmount * sensorAmount * valuesPrSensor;
+//const int len = arduinoAmount * sensorAmount * valuesPrSensor;
 
 unsigned char arduino[arduinoAmount][sensorAmount][valuesPrSensor];
 
 unsigned char buffer[256]; //Initializes the buffer we will use for storing the incomming serial data. This will have to be greater than the packetSize
 const int packetSize = 7; //The packet size in bytes, defined in the serial sender, one for SOP one for SEQ one for LEN and however many data bytes you have in the array (in this case 8) and one for CHK
-int howBig = 25;
+//int howBig = 25;
 
 //---------------
 
@@ -49,14 +118,13 @@ void setup()
     }
   }
   
-  
-  Serial.begin(19200);
+  //Serial.begin(19200);
   pinMode(RxD, INPUT);
   pinMode(TxD, OUTPUT);
   masterSetup();
   //wait 1s and flush the serial buffer
   delay(1000);
-  Serial.flush();
+  //Serial.flush();
   blueToothSerial.flush();
 } 
  
@@ -77,43 +145,31 @@ void loop()
       }
     }
   }
-  
   time = millis();
+  float dt = calDT(time);
+  // calculate kalman and set the angles and velocity
+  for(int i = 0; i < 1; i++){
+     ard[i].getValues(i, arduino);
+     ard[i].setAngles(dt);
+     ard[i].velocity(dt);
+   }
+  // 0 is gyro and 1 is acc
   
-  if(time > lastTime + 20){
-    lastTime = time;
-    unsigned char data[len]; //Putting the values into the data arrgyroY, expand or decrease as needed
-    int count = 0;
-    for(int a = 0; a < arduinoAmount; a++){
-      for(int s = 0; s < sensorAmount; s++){
-        for(int v = 0; v < valuesPrSensor; v++){
-          data[count] = arduino[a][s][v];
-          /*
-          Serial.print(arduino[a][s][v]);
-          Serial.print(' ');
-          //Serial.print(data[count]);
-          //Serial.print("    ");
-          //*/
-          count++;
-        }
-      }
-    }
-    //Serial.println();
-    /*
-    const unsigned int myBufferSize = 260; //myBufferSize must be greater than the entire size of the package
-    unsigned char buffer[myBufferSize]; //creates the array, buffer, of size myBufferSize
-   
-    int bytesWritten = bap::createPacket(buffer, data,len, seq++); //creates the package and returns the size of the package
-    
-    Serial.write(buffer, bytesWritten); //sends the packet as a series of bytes with the length of bytesWritten
-    //*/
+  
+  
+    // send the midi data
     MIDI.sendControlChange(20, data[0], 1);
-    MIDI.sendControlChange(20, data[0], 1);
-    MIDI.sendControlChange(20, data[0], 1);
-    MIDI.sendControlChange(20, data[0], 1);
+    //MIDI.sendControlChange(21, data[0], 1);
+    //MIDI.sendControlChange(22, data[0], 1);
+    //MIDI.sendControlChange(23, data[0], 1);
   }
   
-} 
+
+float calDT(unsigned long time){
+  float dt = time - oldTime;  
+  oldTime = time;
+  return dt;
+  }	
  
 void masterSetup()
 {
@@ -132,52 +188,6 @@ void masterSetup()
     
   //blueToothSerial.print("\r\n+CONN=0,18,E4,C,67,FA\r\n");
   blueToothSerial.print("\r\n+CONN=aa,bb,cc,dd,ee,ff\r\n");  
-  
-  //find the target slave
-  /*char recvChar;
-  while(1){
-    if(blueToothSerial.available()){
-      //Serial.println("I saw something!");
-      recvChar = blueToothSerial.read();
-      Serial.print(recvChar);
-      recvBuf += recvChar;
-      nameIndex = recvBuf.indexOf(slaveName);//get the position of slave name
-      //nameIndex -= 1;//decrease the ';' in front of the slave name, to get the position of the end of the slave address
-      if ( nameIndex != -1 ){
-        //Serial.print(recvBuf);
- 	addrIndex = (recvBuf.indexOf(retSymb,(nameIndex - retSymb.length()- 18) ) + retSymb.length());//get the start position of slave address	 		
- 	slaveAddr = recvBuf.substring(addrIndex, nameIndex);//get the string of slave address 			
- 	break;
-      }
-    }
-  }
-  //form the full connection command
-  connectCmd += slaveAddr;
-  connectCmd += "\r\n";
-  int connectOK = 0;
-  Serial.print("Connecting to slave:");
-  Serial.print(slaveAddr);
-  Serial.println(slaveName);
-  //connecting the slave till they are connected
-  do{
-    blueToothSerial.print(connectCmd);//send connection command
-    recvBuf = "";
-    while(1){
-      if(blueToothSerial.available()){
-        recvChar = blueToothSerial.read();
- 	recvBuf += recvChar;
- 	if(recvBuf.indexOf("CONNECT:OK") != -1){
-          connectOK = 1;
- 	  Serial.println("Connected!");
- 	  blueToothSerial.print("Connected!");
- 	  break;
- 	}else if(recvBuf.indexOf("CONNECT:FAIL") != -1){
- 	  Serial.println("Connect again!");
- 	  break;
- 	}
-      }
-    }
-  }while(0 == connectOK);*/
 }
  
 
