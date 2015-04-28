7de6a902bea4a6e9906e974141d5438baecbbd29
NikolajAndersson <nikolajandersson@gmail.com>
2015-04-28 15:47:57 +0200

    added bluetooth slave code

--- Arduino sketches/Bluetooth_AccGyro/Bluetooth_AccGyro.ino
+++ Arduino sketches/Bluetooth_AccGyro/Bluetooth_AccGyro.ino
@@ -22,9 +22,7 @@ void setup()
   accelemeter.init();
 }
  
-void loop() 
-{ 
-  
+void loop() { 
   float gyroX,gyroY,gyroZ;
   unsigned char GX, GY, GZ, AX, AY, AZ;
   gyro.getAngularVelocity(&gyroX,&gyroY,&gyroZ);
@@ -54,32 +52,32 @@ void loop()
   accZ = int((accZ / 1.52 + 1) * 127);
     AZ = char(accZ);
      
-   unsigned char s[7] = {ArduinoNumber, GX, GY, GZ, AX, AY, AZ};
-  for(int i = 0; i < 7; i++){
+   unsigned char s[6] = {GX, GY, GZ, AX, AY, AZ};
+  /*for(int i = 0; i < 6; i++){
     Serial.print(s[i]);  
     Serial.print(" "); 
-  }Serial.println("");
-   blueToothSerial.write(s, 7);
-  
+  }Serial.println("");*/
+   blueToothSerial.write(s, 6);
    delay(20);
 } 
  
-void slaveSetup()
-{
+void slaveSetup(){
+  
   blueToothSerial.begin(38400); //Set BluetoothBee BaudRate to default baud rate 38400
   blueToothSerial.print("\r\n+STWMOD=0\r\n"); //set the bluetooth work in slave mode
   blueToothSerial.print("\r\n+STBD=38400\r\n"); //tell the bluetooth to communicate at baudrate 38400
   blueToothSerial.print("\r\n+STNA=slave\r\n"); //set the bluetooth name as "slave"
-  blueToothSerial.print("\r\n+STAUTO=0\r\n"); // Auto-connection should be forbidden here
+  blueToothSerial.print("\r\n+STAUTO=1\r\n"); // Auto-connection should be forbidden here
   blueToothSerial.print("\r\n+STOAUT=1\r\n"); // Permit Paired device to connect me
   blueToothSerial.print("\r\n+STPIN=0000\r\n"); //Set SLAVE pincode"0000"
   delay(2000); // This delay is required.
   Serial.println("Starting INQ in 1 sec");
   delay(1000);
   blueToothSerial.print("\r\n+INQ=1\r\n"); //make the slave bluetooth inquirable 
+  blueToothSerial.print("\r\n+RTINQ=aa,bb,cc,dd,ee,ff;slave\r\n");
   Serial.println("The slave bluetooth is inquirable!");
   delay(2000); // This delay is required.
-  blueToothSerial.print("\r\n+RTINQ=aa,bb,cc,dd,ee,ff;slave\r\n");
+  //blueToothSerial.print("\r\n+RTINQ=aa,bb,cc,dd,ee,ff;slave\r\n");
   blueToothSerial.flush();
 }
 
